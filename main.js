feba1bcf4b4a1046840c0f105447261b70e0fc83
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-06 16:13:18 +0530

    special anime

--- main.js
+++ main.js
@@ -25,3 +25,28 @@ var brandSwiper = new Swiper(".brand-swiper", {
         },
     },
 });
+
+// anime swiper
+
+var animeSwiper = new Swiper(".anime-swiper", {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        463: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+        },
+        763: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        },
+        993: {
+          slidesPerView: 4,
+          spaceBetween: 24,
+        },
+        1203: {
+          slidesPerView: 5,
+          spaceBetween: 24,
+        },
+    },
+});
