1b05f839f980c3669c3ec7f808b6ecb0897f16ff
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-02-09 19:02:47 +0530

    project completed

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 
+// show menu
+const navMenu = document.getElementById("nav__menu");
+const navToggle = document.getElementById("nav__toggle");
+const navClose = document.getElementById("nav__close");
+
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show__menu');
+  });
+}
+
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show__menu');
+  });
+}
+
 // scroll header
 function scrollHeader(){
   const header = document.getElementById('header');
